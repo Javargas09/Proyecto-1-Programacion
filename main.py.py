45fb04a6e0a9e7502753ac5f0beb70189587b460
JasonCarvajal <jcarvajal60053@ufide.ac.cr>
2023-03-08 23:18:33 -0600

    Digitar nombre y fecha y validación de ambos

--- main.py.py
+++ main.py.py
@@ -8,9 +8,24 @@ while accion != 2:
     print("2. Salir ")
     
     accion = int(input("Seleccione una acción: "))
-
     if accion == 1:
-        comprar= int(input("Ingresar nueva compra: "))
+        #comprar= int(input("Ingresar nueva compra: "))
+        nombreCliente=input('Digite su nombre: ')
+        while nombreCliente == "":
+            nombreCliente= input('El nombre no puede estar vacio, digite nuevamente su nombre: ')
+                               
+        dia=int(input('Digite el dia: '))
+        mes=int(input('Digite el mes: '))
+        año=int(input('Digite al año: '))
+        if dia > 31 or mes> 12 or año < 2022:
+            print('FECHA INVALIDA , el dia no puede ser mayor a 31 ,  el mes mayor a 12 o el año menos al 2022 ')
+            dia=int(input('Digite el dia: '))
+            mes=int(input('Digite el mes: '))
+            año=int(input('Digite al año: '))
+        else:
+            print('La compra de '+nombreCliente+'se registro correctamente el '+str(dia)+'/'+str(mes)+'/'+str(año))
+
+            
     elif accion == 2:
         salir= int(input("Saliendo del programa..."))
     else:
