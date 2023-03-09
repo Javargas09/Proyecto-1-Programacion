f2dff2eb86b35d502225d63dfe67ea38a03622ec
tomesme <testrada10660@ufide.ac.cr>
2023-03-08 19:48:08 -0600

    menu

--- main.py.py
+++ main.py.py
@@ -1,1 +1,17 @@
 print("prueba")
+
+accion = 0
+
+while accion != 2:
+    print("Menu:")
+    print("1. Ingresar nueva compra ")
+    print("2. Salir ")
+    
+    accion = int(input("Seleccione una acción: "))
+
+    if accion == 1:
+        comprar= int(input("Ingresar nueva compra: "))
+    elif accion == 2:
+        salir= int(input("Saliendo del programa..."))
+    else:
+        print("Opción inválida. Intente de nuevo.")
