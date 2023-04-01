fcc595816ed4a0deccaeccef3944101697e8bc58
JasonCarvajal <jcarvajal60053@ufide.ac.cr>
2023-03-31 19:14:32 -0600

    Creacion metodo descuentoo,calculo del precio, IVA y descuento

--- main.py.py
+++ main.py.py
@@ -1,7 +1,21 @@
-
 accion = 0
 actual = 0
-while accion != 3:
+def descuento(cantidad,precio):
+    precioIva=precio*0.13
+
+    if cantidad <= 5:
+        precioDescuento=precio-(precio * 0.02)
+    elif cantidad > 5 and cantidad<=10:
+        precioDescuento=precio-(precio * 0.05)
+    elif cantidad > 10:
+        precioDescuento=precio-(precio * 0.07)
+
+        precioNeto=precioDescuento+precioIva
+
+    print('El precio total con descuento es: '+str(precioDescuento))
+    print('El precio neto es : '+str(precioNeto))
+
+while accion != 2:
     print("Menu:")
     print("1. Ingresar nueva compra ")
     print("2. Salir ")
@@ -11,7 +25,6 @@ while accion != 3:
         nombreCliente=input('Digite su nombre: ')
         while nombreCliente == "":
             nombreCliente= input('El nombre no puede estar vacio, digite nuevamente su nombre: ')
-            print('Cambios Jason')
                                
         dia=int(input('Digite el dia: '))
         while dia < 1 or dia > 31:
@@ -34,47 +47,65 @@ while accion != 3:
         print('[4]Atun trocitos(140gr) Precio:1500')
 
 
-    precio = 0
-    bandera=True
-    opcion=1
-    listaCompra=""
+        precio = 0
+        cantidadTotal=0
+        bandera=True
+        opcion=1
+        listaCompra=""
+    
+        contador=1
+        #CAMBIAR UN 4 POR EL 20. EL 4 ES SOLO PARA PROBAR
+        while bandera and contador <= 4:
+            cantidad=0
+            producto =int(input(' Seleccione un producto digitando el número del mismo '))
+            cantidad = int(input("Ingrese la cantidad que desea comprar: "))
 
-    while bandera:
-        cantidad=0
-        producto =int(input(' Seleccione un producto digitando el número del mismo '))
-        cantidad = int(input("Ingrese la cantidad que desea comprar: "))
+            if  producto == 1:
+                precioProducto=900* cantidad
+                listaCompra+=("Aceite de Soya: "+str(cantidad)+"   "+str(precioProducto)+'\n')
+                precio += precioProducto
+                cantidadTotal += cantidad
+                        
+            elif producto == 2:
+                precioProducto=1200 *cantidad
+                listaCompra+=("Agua: "+str(cantidad)+"   "+str(precioProducto)+'\n')
+                precio += precioProducto
+                cantidadTotal += cantidad
 
-        if  producto == 1:
-            precioProducto=900* cantidad
-            listaCompra+=("Aceite de Soya: "+str(cantidad)+"   "+str(precioProducto)+'\n')
-            precio += precioProducto
+
+            elif producto == 3:
+                precioProducto=1000 *cantidad
+                listaCompra+=("Arroz: "+str(cantidad)+"   "+str(precioProducto)+'\n')
+                precio += precioProducto
+                cantidadTotal += cantidad  
+
+            elif producto ==  4:
+                precioProducto=1500 *cantidad
+                listaCompra+=("Atun trocitos: "+str(cantidad)+"   "+str(precioProducto)+'\n')
+                precio += precioProducto
+                cantidadTotal += cantidad
+            else:
+                producto=int(print('Producto no existente, porfavor digite un producto existente'))
+
+            contador += 1
+            if contador <= 4:
+                respuesta=input('Desea realizar otra compra? |Y| para si o |N| para no ')
+                #CAMBIAR EL 5 POR UN 21. 5 SOLO PARA PROBAR
+                if respuesta == 'N' or contador == 5:
+                    print("============Factura============")
+                    print(listaCompra)
+                    print('El precio total es: ' + str(precio))       
+                    #print("La cantidad total de productos es: "+str(cantidadTotal)) 
+                    descuento(cantidadTotal, precio)
                     
-        elif producto == 2:
-           precioProducto=1200 *cantidad
-           listaCompra+=("Agua: "+str(cantidad)+"   "+str(precioProducto)+'\n')
-           precio += precioProducto
+                    bandera=False
+            else:
+                print("Ha alcanzado el limite de 20 productos en la factura.Realice una nueva compra")
+                print(listaCompra)
+                print("El precio total es: " + str(precio))
+                descuento(cantidadTotal, precio)
+                #print("La cantidad total de productos es: "+str(cantidadTotal))
 
-
-        elif producto == 3:
-           precioProducto=1000 *cantidad
-           listaCompra+=("Arroz: "+str(cantidad)+"   "+str(precioProducto)+'\n')
-           precio += precioProducto
-                    
-        elif producto ==  4:
-            precioProducto=1500 *cantidad
-            listaCompra+=("Atun trocitos: "+str(cantidad)+"   "+str(precioProducto)+'\n')
-            precio += precioProducto
-        else:
-            producto=int(print('Producto no existente, porfavor digite un producto existente'))
-
-        respuesta=input('Desea realizar otra compra? |Y| para si o |N| para no ')
-        if respuesta == 'N':
-            print("============Factura============")
-            print(listaCompra)
-            print('El precio total es: '+str(precio))
-                    
-            bandera=False         
+                bandera=False        
     if accion == 2:
         salir= int(input("Saliendo del programa..."))
-    else:
-        print("Opción inválida. Intente de nuevo.")
